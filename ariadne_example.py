ba2e3a182e7fcf06ffe57c731d2567edda89bca3
W. Matthew Wilson <mwilson@level20.com>
2020-05-11 18:29:49 -0400

    WIP

--- ariadne_example.py
+++ ariadne_example.py
@@ -6,11 +6,12 @@ import textwrap
 import psycopg2
 from ariadne import load_schema_from_path
 from ariadne import ObjectType
-from ariadne.asgi import GraphQL
+from ariadne.wsgi import GraphQL
+from ariadne import make_executable_schema
 
 query = ObjectType("Query")
 
-@query.field("upcoming_shows")
+# @query.field("upcoming_shows")
 def resolve_upcoming_shows(obj, info):
 
     print(info.context)
@@ -26,9 +27,18 @@ def get_upcoming_shows(pgconn):
 
     return cursor
 
+@query.field("demo")
+def resolve_demo_query(_, info):
+
+    return dict(
+        current_timestamp="placeholder current_timestamp...",
+        bogus_field_a="AAA",
+        bogus_field_b="BBB")
+
 @query.field("current_timestamp")
 def resolve_current_timestamp(_, info):
 
+        pgconn = info.context["pgconn"]
         cursor = pgconn.cursor()
         cursor.execute("select current_timestamp")
         return cursor.fetchone()[0]
@@ -42,7 +52,9 @@ def build_app():
 
         pgconn = psycopg2.connect(os.environ["PGCONN"])
 
-        schema = load_schema_from_path("bogus_schema.graphql")
+        schema = make_executable_schema(
+            load_schema_from_path("bogus_schema.graphql"),
+            query)
 
         app = GraphQL(schema, debug=True, context_value=dict(pgconn=pgconn))
 
